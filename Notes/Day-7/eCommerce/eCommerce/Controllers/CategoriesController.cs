b2294288bff39a8edecc6568b779acfb1ef89493
Prasad-R-Patil <prasadpatil24212@gmail.com>
2025-06-25 16:21:59 +0530

    cmt

---
+++ Notes/Day-7/eCommerce/eCommerce/Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using eCommerce.Models;
+using eCommerce.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.Controllers;
+
+public class CategoriesController : Controller
+{
+    private readonly ICommonRepository<Category> _categoryRepository;
+
+    public CategoriesController(ICommonRepository<Category> categoryRepository)
+    {
+        _categoryRepository = categoryRepository;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        ViewBag.PageTitle = "eCommerce Categories List!";
+        var categories = await _categoryRepository.GetAllAsync();
+        return View(categories);
+    }
+}
