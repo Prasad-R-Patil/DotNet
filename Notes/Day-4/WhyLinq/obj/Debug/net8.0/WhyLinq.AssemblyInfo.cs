c918291f77da2497551cb7a62aa81690aa8e9077
Prasad-R-Patil <prasadpatil24212@gmail.com>
2025-07-01 23:02:27 +0530

    cmt

--- Notes/Day-4/WhyLinq/obj/Debug/net8.0/WhyLinq.AssemblyInfo.cs
+++ Notes/Day-4/WhyLinq/obj/Debug/net8.0/WhyLinq.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WhyLinq")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3f970eb1fe872889a37f657c69a891f363ca780")]
 [assembly: System.Reflection.AssemblyProductAttribute("WhyLinq")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WhyLinq")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
